ceaf576e5ddf9cf44fee1fc6bf44095090fb89e2
Tanya Tsodikovich <tany999m@gmail.com>
2023-01-01 23:24:39 +0200

    feat: create helper

---
+++ helpers/api.js
@@ -0,0 +1,35 @@
+const axios = require('axios');
+
+const getData = (req, res) => {
+    const API_KEY = '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'
+    const prompt = req.params.text;
+
+    const data = {
+        model: "text-davinci-003",
+        prompt: `Summarize this into 3 bullet points to be added to my resume:\n\n${prompt}`,
+        temperature: 0.7,
+        max_tokens: 256,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0,
+    }
+
+    axios.post('https://api.openai.com/v1/completions', data, {
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${API_KEY}`
+        }
+    })
+        .then((response) => {
+            console.log(response.data)
+            res.send(response.data.choices[0].text)
+        })
+        .catch((error) => {
+            console.log(error)
+            res.send(error)
+        })
+}
+
+module.exports = {
+    getData
+}
--- routes/api.js
+++ routes/api.js
@@ -1,36 +1,8 @@
 var express = require('express');
+const { getData } = require('../helpers/api');
 var router = express.Router();
-const axios = require('axios');
 
 /* GET input listing. */
-router.get('/input/:text', function (req, res, next) {
-  const API_KEY = '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'
-  const prompt = req.params.text;
-
-  const data = {
-    model: "text-davinci-003",
-    prompt: `Summarize this into 3 bullet points to be added to my resume:\n\n${prompt}`,
-    temperature: 0.7,
-    max_tokens: 256,
-    top_p: 1,
-    frequency_penalty: 0,
-    presence_penalty: 0,
-  }
-
-  axios.post('https://api.openai.com/v1/completions', data, {
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${API_KEY}`
-    }
-  })
-    .then((response) => {
-      console.log(response.data)
-      res.send(response.data.choices[0].text)
-    })
-    .catch((error) => {
-      console.log(error)
-      res.send(error)
-    })
-});
+router.get('/input/:text', getData);
 
 module.exports = router;
