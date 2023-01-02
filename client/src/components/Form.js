3eb5c03bd3dc37caca85a600153ecaa80011edac
Tanya Tsodikovich <tany999m@gmail.com>
2023-01-02 22:50:37 +0200

    feat: display the data in a paragraph at the client

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -1,34 +1,36 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 function Form() {
     const [inputText, setInputText] = useState("");
     const [output, setOutput] = useState("");
 
     const URL = 'http://localhost:4000/api/input/'
-    
-    const getData = async (text) => {
+
+    const getData = (text) => {
         fetch(`${URL}${text}`)
-        .then((response) => response.json())
-        .then((data) => console.log(data));
+            .then((response) => response.json())
+            .then((res) => setOutput(res.data));
     }
 
-    
-    const handleSubmit = async (evt) => {
+
+    const handleSubmit = (evt) => {
         evt.preventDefault();
-        const data = await getData(inputText);
-        setOutput(data);
+        getData(inputText);
     }
 
     return (
-        <form className="form" onSubmit={handleSubmit}>
-            <label className="label">
-                Pick a past job and enter your experience here:
-                <textarea className="input" id="text" name="text" cols="100" rows="20" 
-                    onChange={(evt) => setInputText(evt.target.value)} >
-                </textarea>
-            </label>
-            <input className="button" type="submit" value="Submit" />
-        </form>
+        <>
+            <form className="form" onSubmit={handleSubmit}>
+                <label className="label">
+                    Pick a past job and enter your experience here:
+                    <textarea className="input" id="text" name="text" cols="100" rows="20"
+                        onChange={(evt) => setInputText(evt.target.value)} >
+                    </textarea>
+                </label>
+                <input className="button" type="submit" value="Submit" />
+            </form>
+            <p className="output">{output}</p>
+        </>
     )
 }
 
