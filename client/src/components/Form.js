7eccbc7c3c4454dfeb9c80fca43ff322178b09ea
Tanya Tsodikovich <tany999m@gmail.com>
2023-01-02 22:27:57 +0200

    feat: add call for api from the client side

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -1,9 +1,30 @@
+import React, {useState} from 'react';
+
 function Form() {
+    const [inputText, setInputText] = useState("");
+    const [output, setOutput] = useState("");
+
+    const URL = 'http://localhost:4000/api/input/'
+    
+    const getData = async (text) => {
+        fetch(`${URL}${text}`)
+            .then(response => console.log(response))
+    }
+
+    
+    const handleSubmit = async (evt) => {
+        evt.preventDefault();
+        const data = await getData(inputText);
+        setOutput(data);
+    }
+
     return (
-        <form className="form">
+        <form className="form" onSubmit={handleSubmit}>
             <label className="label">
                 Pick a past job and enter your experience here:
-                <textarea className="input" id="text" name="text" cols="100" rows="20" ></textarea>
+                <textarea className="input" id="text" name="text" cols="100" rows="20" 
+                    onChange={(evt) => setInputText(evt.target.value)} >
+                </textarea>
             </label>
             <input className="button" type="submit" value="Submit" />
         </form>
